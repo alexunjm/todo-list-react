b04f210e01be937a6d5e35f71b4fd3bcaeb07082
alexanderjaramillo <alexunjm@gmail.com>
2020-08-06 01:15:51 -0500

    visual unstyled basic components

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Footer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+
+  render() {
+    return (
+      <div>
+        footer
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+
+  render() {
+    return (
+      <div>
+        header
+      </div>
+    );
+  }
+}
+
+export default Header;
