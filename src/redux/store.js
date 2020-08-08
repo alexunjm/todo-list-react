a559995409db27d02eec1e0e1d7610bdb0424a58
alexanderjaramillo <alexunjm@gmail.com>
2020-08-08 00:42:57 -0500

    logger global redux state off

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
-import { logger } from "./middlewares";
+// import { logger } from "./middlewares";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewareArr = [logger, thunk];
+const middlewareArr = [/* logger,  */thunk];
 
 export default createStore(
   rootReducer,
